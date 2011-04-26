167371aa70702525b91bbbc412400dcc7380cbb2
Matthew Anderson <sublime@sawred.com>
2011-04-26 15:35:09 -0500

    Added Version Selection

--- Tools/Shell Fetcher/fetcher.sh
+++ Tools/Shell Fetcher/fetcher.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+#For the love of god make sure this is only has read access.
+options="\"Drupal 6\" \"Drupal 7\" \"Wordpress\""
+
+echo "Welcome to Fetcher. Where do you want to go today?"
+PS3="Please type the number corresponding to the installation you would like: "
+select i in Drupal-6 Drupal-7 Wordpress-3.1.1 Quit
+ do 
+ [ $i = "Quit" ]  && exit 0
+ echo "You chose $i"
+done
