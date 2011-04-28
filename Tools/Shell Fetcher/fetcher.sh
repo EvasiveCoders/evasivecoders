91bc56950ac36c234ac409d9220e673b71430838
Matthew Anderson <sublime@sawred.com>
2011-04-28 16:23:24 -0500

    Added variables, updated elif

--- Tools/Shell Fetcher/fetcher.sh
+++ Tools/Shell Fetcher/fetcher.sh
@@ -1,13 +1,42 @@
 #!/bin/sh
-
 #For the love of god make sure this is only has read access.
-options="\"Drupal 6\" \"Drupal 7\" \"Wordpress\""
-
+#
+#
+#Set Version Veriables here for easy updating -- no spaces!
+D6="Drupal-6.20"
+D7="Drupal-7.0"
+WP="Wordpress-Latest"
+##URLS
+D6DL="http://ftp.drupal.org/files/projects/drupal-6.20.tar.gz"
+D7DL="http://ftp.drupal.org/files/projects/drupal-7.0.tar.gz"
+WPDL="http://wordpress.org/latest.tar.gz"
+#######
 echo "Welcome to Fetcher. Where do you want to go today?"
+echo "Installation Directory (public_html):"
+read installdir
+echo "Database Name:"
+read dbname
+echo "Database User:"
+read dbuser
+echo "Database Pass:"
+read dbpass
 PS3="Please type the number corresponding to the installation you would like: "
-select i in Drupal-6 Drupal-7 Wordpress-3.1.1 Quit
- do 
- [ $i = "Quit" ]  && exit 0
- echo "You chose $i"
->>>>>>> 0eed8b3f79d09b67c6c31ed7d63126f28f1c9ccf
+select i in $D6 $D7 $WP Quit
+do 
+if [ $i = "Quit" ];
+then
+exit
+# Drupal 7
+elif [ $i = $D7 ];
+then
+echo "Oh yeah"
+# Drupal 6
+elif [ $i = $D6 ];
+then
+echo "Oh yeah"
+# Wordpress
+elif [ $i = $WP ];
+then
+echo "Oh yeah"
+fi
 done
